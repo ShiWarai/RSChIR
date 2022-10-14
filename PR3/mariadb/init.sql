7d866b1f8c2b9d8479625388a22e065ff9aedc84
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-10-14 04:07:14 +0300

    Feat: add 3rd work

--- PR3/mariadb/init.sql
+++ PR3/mariadb/init.sql
@@ -1,42 +1,46 @@
 CREATE DATABASE IF NOT EXISTS appDB;
+ALTER DATABASE appDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
 GRANT SELECT,UPDATE,INSERT,DELETE ON appDB.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 
 USE appDB;
-CREATE TABLE IF NOT EXISTS users (
-  ID INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(20) NOT NULL,
-  surname VARCHAR(40) NOT NULL,
-  PRIMARY KEY (ID)
+SET NAMES UTF8;
+
+CREATE TABLE IF NOT EXISTS toy (
+    id INT NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    description TEXT,
+    price INT NOT NULL,
+    count INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
+INSERT INTO toy (id, name, price, count)
+VALUES  (0, 'пистолетик', 1200, 120),
+        (1, 'кукла', 1000, 432),
+        (2, 'игрушечный компьютер', 5400, 32);
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Bob', 'Marley') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Bob' AND surname = 'Marley'
-) LIMIT 1;
+CREATE TABLE IF NOT EXISTS purchase (
+   id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(64) NOT NULL,
+   toy_id INT NOT NULL,
+   FOREIGN KEY (toy_id) REFERENCES toy(id),
+   wholesale_price INT NOT NULL,
+   count INT NOT NULL,
+   PRIMARY KEY (ID)
+);
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Alex', 'Rover') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Alex' AND surname = 'Rover'
-) LIMIT 1;
+INSERT INTO purchase (name, toy_id, wholesale_price, count)
+VALUES ('закупка №43', 0, 110, 100),
+       ('закупка №44', 2, 400, 10);
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Kate', 'Yandson') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Kate' AND surname = 'Yandson'
-) LIMIT 1;
+CREATE TABLE IF NOT EXISTS user (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    login VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(33) NOT NULL,
+    PRIMARY KEY(id)
+);
 
-INSERT INTO users (name, surname)
-SELECT * FROM (SELECT 'Lilo', 'Black') AS tmp
-WHERE NOT EXISTS (
-    SELECT name FROM users WHERE name = 'Lilo' AND surname = 'Black'
-) LIMIT 1;
+INSERT INTO user (login, password)
+VALUES ('admin', '{SHA}0DPiKuNIrrVmD8IUCuw1hQxNqZc=');
