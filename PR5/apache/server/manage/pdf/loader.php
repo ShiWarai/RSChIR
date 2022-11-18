dbd07042932b3036468a8c1b1fa5c6a22c968244
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-11-18 17:15:36 +0300

    Fix: add real PDF checking

--- PR5/apache/server/manage/pdf/loader.php
+++ PR5/apache/server/manage/pdf/loader.php
@@ -5,7 +5,7 @@ $uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
 echo '<pre>';
 setlocale(LC_ALL,'en_US.UTF-8');
 $ext = pathinfo($_FILES['userfile']['name'], PATHINFO_EXTENSION);
-if ($ext != "pdf") {
+if ($ext != "pdf" || !str_contains(file_get_contents($_FILES['userfile']['tmp_name']), "%PDF-")) {
     echo "Вы попытались загрузить не pdf файл";
 } else {
     if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
