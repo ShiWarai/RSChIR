c009ec6c5d73e9d1ee602f92d907dee3c400e621
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-11-28 05:45:21 +0300

    Build: init new task

---
+++ PR6/apache/server/manage/purchases.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../session.php";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Закупки</title>
+    <link rel="stylesheet" href="/style.css" type="text/css"/>
+</head>
+<body>
+<h1>Список закупок</h1>
+<table>
+    <tr><th>Имя</th><th>ID товара</th><th>Оптовая цена</th><th>Кол-во</th></tr>
+    <?php
+    $mysqli = new mysqli("db", "user", "password", "appDB");
+    $result = $mysqli->query("SELECT * FROM purchase");
+    foreach ($result as $row){
+        echo "<tr><td>{$row['name']}</td><td>{$row['toy_id']}</td><td>{$row['wholesale_price']} р.</td><td>{$row['count']}</td></tr>";
+    }
+    ?>
+</table>
+</body>
+</html>
---
+++ PR6/apache/server/toys.php
@@ -0,0 +1,24 @@
+<?php
+require_once "session.php";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Игрушки</title>
+    <link rel="stylesheet" href="/style.css" type="text/css"/>
+</head>
+<body>
+<h1>Список игрушек</h1>
+<table>
+    <tr><th>Id</th><th>Имя</th><th>Цена</th></tr>
+<?php
+$mysqli = new mysqli("db", "user", "password", "appDB");
+$result = $mysqli->query("SELECT * FROM toy");
+foreach ($result as $row){
+    echo "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['price']} р.</td></tr>";
+}
+?>
+</table>
+</body>
+</html>
